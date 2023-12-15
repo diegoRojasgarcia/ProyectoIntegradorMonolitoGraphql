98a885223572f730867226413e03fb14a0e14e76
Mekaizer <mekaizerzx@gmail.com>
2023-12-15 18:53:11 -0300

    cambios 2

---
+++ golang/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func main() {
+	// Conectarse a la base de datos
+	db, err := sql.Open("mysql", "postgress:ZeroZX@tcp(localhost:3306)/DataBaseGraphql")
+	if err != nil {
+		panic(err)
+	}
+
+	// Ejecutar la consulta
+	rows, err := db.Query("SELECT * FROM product where id = 1")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	// Iterar sobre las filas de la consulta
+	for rows.Next() {
+		// Obtener los datos de la fila
+		var id int
+		var name string
+
+		err := rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+
+		// Imprimir los datos de la fila
+		fmt.Println(id, name)
+	}
+}
