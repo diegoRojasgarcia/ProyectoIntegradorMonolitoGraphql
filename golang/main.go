54205aae15e1036b6f9d2e6acfe1926f4498095e
Mekaizer <mekaizerzx@gmail.com>
2023-12-15 21:07:21 -0300

    golang muestra el top 3 por ranking de productos

--- golang/main.go
+++ golang/main.go
@@ -9,30 +9,36 @@ import (
 
 func main() {
 
-	// Conectarse a la base de datos
-	db, err := sql.Open("mysql", "postgress:ZeroZX@tcp(localhost:3306)/DataBaseGraphql")
+	// Conectarse a la base de datos  5432
+	connectionString := "host=localhost port=5432 user=postgres password=ZeroZX dbname=DataBaseGraphql  sslmode=disable"
+
+	db, err := sql.Open("postgres", connectionString)
+
 	if err != nil {
 		panic(err)
 	}
+	defer db.Close()
 
-	// Ejecutar la consulta
-	rows, err := db.Query("SELECT * FROM product where id = 1")
+	rows, err := db.Query("SELECT id, name, description, price, rank, state FROM product ORDER BY rank DESC LIMIT 3")
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
-	// Iterar sobre las filas de la consulta
 	for rows.Next() {
 		// Obtener los datos de la fila
-		var id int
+		var id int64
 		var name string
+		var description string
+		var price int
+		var rank float64
+		var state string
 
-		err := rows.Scan(&id, &name)
+		err := rows.Scan(&id, &name, &description, &price, &rank, &state)
 		if err != nil {
 			panic(err)
 		}
 
-		// Imprimir los datos de la fila
-		fmt.Println(id, name)
+		// Mostrar los datos de la fila
+		fmt.Println(name, price, rank, state)
 	}
 }
